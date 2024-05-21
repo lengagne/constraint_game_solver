edd9a04109e6e0a0765be030c46b158ff4270608
Sébastien Lengagne <lengagne@gmail.com>
2024-05-21 19:56:45 +0200

    first commit

---
+++ apps/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{       
+        std::cout<<"Constraint Problem Solver"<<std::endl;
+        std::cout<<"Please refer a kind of problem (Cluedo, Sudoku, ...)"<<std::endl;
+        
+        return 0;
+} 
